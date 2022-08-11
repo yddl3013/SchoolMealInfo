aa386b80441479367469486b71a899379b1dfbb2
yddl3013 <gominjae47@gmail.com>
2022-08-11 19:04:17 +0900

    디테일 수정

--- diet.js
+++ diet.js
@@ -1,14 +1,39 @@
-let today = new Date();
+let get = new Date();
 
 let currentDate = document.getElementsByClassName("currentDate");
 
-let todayYear = today.getFullYear();
-let todayMonth = today.getMonth();
-let todayDate = today.getDate();
+let getYear = get.getFullYear();
+let getMonth = get.getMonth();
+let getDate = get.getDate();
+let getDay = get.getDay();
+let currentDay = '';
 
+switch (getDay) {
+    case 0:
+        currentDay = '일'
+        break;
+    case 1:
+        currentDay = '월'
+        break;
+    case 2:
+        currentDay = '화'
+        break;
+    case 3:
+        currentDay = '수'
+        break;
+    case 4:
+        currentDay = '목'
+        break;
+    case 5:
+        currentDay = '금'
+        break;
+    case 3:
+        currentDay = '토'
+        break;
+}
 
 function insertDate() {
-    return `${todayYear}/${todayMonth+1}/${todayDate}`
+    return `${getYear}/${getMonth+1}/${getDate}/${currentDay}`
 };
 
 currentDate[0].textContent = insertDate();
