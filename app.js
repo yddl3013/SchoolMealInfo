ab078db51bf9e76875c077d28066fa528536b8b7
yddl3013 <gominjae47@gmail.com>
2022-08-12 12:45:48 +0900

    https 보안 관련 업데이트

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
 
+const https = require('https');
+const PORT = process.env.PORT || 443;
+
+const server = https.createServer(app);
+const options = {
+	key: fs.readFileSync(__dirname + '/home/ubuntu/SchoolMealInfo/server.key'),
+    cert: fs.readFileSync(__dirname + '/home/ubuntu/SchoolMealInfo/server.crt'),
+    ca: fs.readFileSync(__dirname + '/home/ubuntu/SchoolMealInfo/server.csr'),
+};
+
+https.createServer(options, app).listen(PORT);
+
 app.use(express.static(__dirname + ''))
 
 app.get('/', function(req,res) {
     res.sendFile(__dirname + '/diet.html')
 })
 
-app.listen(3000, function() {
-    console.log('express server on 3000')
+app.listen(80, function() {
+    console.log('express server on 80')
 })
