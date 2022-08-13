da662944a4543e603d2ace51ffb6df7482f7c55d
yddl3013 <gominjae47@gmail.com>
2022-08-13 22:39:38 +0900

    오류 수정

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const express = require('express')
 const app = express()
 
-const https = require('https');
-const PORT = process.env.PORT || 443;
+// const https = require('https');
+// const PORT = process.env.PORT || 443;
 
-const server = https.createServer(app);
-const options = {
-	key: fs.readFileSync(__dirname + '/home/ubuntu/SchoolMealInfo/server.key'),
-    cert: fs.readFileSync(__dirname + '/home/ubuntu/SchoolMealInfo/server.crt'),
-    ca: fs.readFileSync(__dirname + '/home/ubuntu/SchoolMealInfo/server.csr'),
-};
+// const server = https.createServer(app);
+// const options = {
+// 	key: fs.readFileSync(__dirname + '/server.key'),
+//     cert: fs.readFileSync(__dirname + '/server.crt'),
+//     ca: fs.readFileSync(__dirname + '/server.csr'),
+// };
 
-https.createServer(options, app).listen(PORT);
+// https.createServer(options, app).listen(PORT);
 
 app.use(express.static(__dirname + ''))
 
