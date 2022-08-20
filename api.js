bb158e8a4371deea49786e184a16ab269356d001
yddl3013 <gominjae47@gmail.com>
2022-08-21 00:42:19 +0900

    버그 수정 및 날짜 변경 기능 추가

--- api.js
+++ api.js
@@ -1,72 +1,74 @@
-const url = `https://open.neis.go.kr/hub/mealServiceDietInfo`;
-const serviceKey = '111f48f0039644d087e84073ded0515d';
+//전역변수
+const url = `https://open.neis.go.kr/hub/mealServiceDietInfo`
+const serviceKey = '111f48f0039644d087e84073ded0515d'
 
 // childElement를 넣고싶은 부모 태그
-const parentElement = document.querySelector('.currentDiet');
-
-
+const parentElement = document.querySelector('.currentDiet')
+let diet
+let diet_arr = []
+//함수
 function pushDate() {
-    let apiYear = getYear;
-    let apiMonth = getMonth;
-    let apiDate = getDate;
+    let apiYear = getYear
+    let apiMonth = getMonth
+    let apiDate = getDate
 
     if (getMonth < 10) {
-       apiMonth = 0+String(getMonth);
-    };
+       apiMonth = 0+String(getMonth)
+    }
 
     if (getDate < 10) {
-       apiDate = 0+String(getDate);
-    };
+       apiDate = 0+String(getDate)
+    }
     
-    return `${apiYear}${apiMonth}${apiDate}`;
+    return `${apiYear}${apiMonth}${apiDate}`
 };
 
-let diet;
-async function getAPI() {
-axios({
-    url: url, 
-    method: 'get',
-    params: {
-        Key: serviceKey,
-        Type: 'json',
-        pIndex: 1,
-        pSize: 10,
-        ATPT_OFCDC_SC_CODE: 'J10',
-        SD_SCHUL_CODE: '7531149',   
-        MLSV_YMD: pushDate()
-    }
-}).then(function (res) {
-    const dataSet = res.data;
-    try {
-        diet = res.data.mealServiceDietInfo[1].row[0].DDISH_NM;
-        diet_arr = diet.split('<br/>');
-        for (let i = 0; i < diet_arr.length; i++) { // 이 아래 과정을 급식의 개수만큼 반복
-            // console.log(diet_arr[i]);
-    
-            // 부모 태그에 넣을 자식 element를 생성 (div태그로)
-            let mealInfoElement = document.createElement('div')
-    
-            // 자식 태그에 diet라는 클래스를 공통으로 넣기 위해서 classList에 add함수를 이용해 diet를 넣음
-            mealInfoElement.classList.add('diet')
-            // 자식 태그에 텍스트를 넣기 위해 diet_arr[i]에 있는 String를 넣음
-            mealInfoElement.textContent = diet_arr[i] 
-    
-            // 부모 태그에 새로 만든 자식 태그를 넣음
-            parentElement.append(mealInfoElement)
+//api 요청
+
+    async function getAPI() {
+        axios({
+            url: url, 
+            method: 'get',
+            params: {
+                Key: serviceKey,
+                Type: 'json',
+                pIndex: 1,
+                pSize: 10,
+                ATPT_OFCDC_SC_CODE: 'J10',
+                SD_SCHUL_CODE: '7531149',   
+                MLSV_YMD: pushDate()
+            }
+        }).then(function (res) {
+            const dataSet = res.data
+            try {
+                diet = res.data.mealServiceDietInfo[1].row[0].DDISH_NM
+                diet_arr = diet.split('<br/>')
+                for (let i = 0; i < diet_arr.length; i++) { // 이 아래 과정을 급식의 개수만큼 반복
+            
+                    // 부모 태그에 넣을 자식 element를 생성 (div태그로)
+                    let mealInfoElement = document.createElement('div')
+                    
+                    // 자식 태그에 diet라는 클래스를 공통으로 넣기 위해서 classList에 add함수를 이용해 diet를 넣음
+                    mealInfoElement.classList.add('diet')
+                    // 자식 태그에 텍스트를 넣기 위해 diet_arr[i]에 있는 String를 넣음
+                    mealInfoElement.textContent = diet_arr[i] 
+            
+                    // 부모 태그에 새로 만든 자식 태그를 넣음
+                    parentElement.append(mealInfoElement)
+                }
+            } catch (error) {
+                console.log("data not found err(식단 정보를 불러올 수 없습니다):: ", error)
+                let mealInfoElement = document.createElement('div')
+            
+                    // 자식 태그에 diet라는 클래스를 공통으로 넣기 위해서 classList에 add함수를 이용해 diet를 넣음
+                    mealInfoElement.classList.add('diet')
+
+                    // 자식 태그에 텍스트를 넣기 위해 diet_arr[i]에 있는 String를 넣음
+                    mealInfoElement.textContent = "식단 정보가 없습니다." 
+            
+                    // 부모 태그에 새로 만든 자식 태그를 넣음
+                    parentElement.append(mealInfoElement)
+            }
+          });
         }
-    } catch (error) {
-        alert('식단 정보가 없습니다.')
-        console.log("data not found err:: ", error)
-        let mealInfoElement = document.createElement('div')
-    
-            // 자식 태그에 diet라는 클래스를 공통으로 넣기 위해서 classList에 add함수를 이용해 diet를 넣음
-            mealInfoElement.classList.add('diet')
-            // 자식 태그에 텍스트를 넣기 위해 diet_arr[i]에 있는 String를 넣음
-            mealInfoElement.textContent = "식단 정보가 없습니다." 
-    
-            // 부모 태그에 새로 만든 자식 태그를 넣음
-            parentElement.append(mealInfoElement)
-    }
-  });
-}   
-getAPI();
+getAPI() 
--- diet.js
+++ diet.js
@@ -1,40 +1,101 @@
-let get = new Date();
+//전역변수
+let get = new Date()
 
-let currentDate = document.getElementsByClassName("currentDate");
+let currentDate = document.getElementsByClassName("currentDate")
 
-let getYear = get.getFullYear();
-let getMonth = get.getMonth() + 1;
-let getDate = get.getDate();
-let getDay = get.getDay();
-let currentDay = '';
+let getYear = get.getFullYear()
+let getMonth = get.getMonth() + 1
+let getDate = get.getDate()
+let getDay = get.getDay()
+let currentDay;
 
-switch (getDay) {
-    case 0:
-        currentDay = '일'
-        break;
-    case 1:
-        currentDay = '월'
-        break;
-    case 2:
-        currentDay = '화'
-        break;
-    case 3:
-        currentDay = '수'
-        break;
-    case 4:
-        currentDay = '목'
-        break;
-    case 5:
-        currentDay = '금'
-        break;
-    case 6:
-        currentDay = '토'
-        break;
+let previousDate = document.getElementsByClassName('previousDate')
+let nextDate = document.getElementsByClassName('nextDate')
+
+
+//함수
+function calDay() {
+    if (getDay == -1) {
+        getDay = 6
+    }
+    if (getDay == 7) {
+        getDay = 0
+    }
+    switch (getDay) {
+        case 0:
+            currentDay = '일'
+            break;
+        case 1:
+            currentDay = '월'
+            break;
+        case 2:
+            currentDay = '화'
+            break;
+        case 3:
+            currentDay = '수'
+            break;
+        case 4:
+            currentDay = '목'
+            break;
+        case 5:
+            currentDay = '금'
+            break;
+        case 6:
+            currentDay = '토'
+            break;
+    }
+    return currentDay
 }
 
-function insertDate() {
-    return `${getYear}/${getMonth}/${getDate}/${currentDay}`
-};
+function inputDate() {
+    return `${getYear}/${getMonth}/${getDate}/${calDay()}`
+}
 
-currentDate[0].textContent = insertDate();
+function calPreDate() {
+    getDate -= 1
+    getDay -= 1
+    calDay()
+    if (getDate == 0) {
+        getMonth -= 1
+        let calDate = new Date(getYear, getMonth, 0)
+        getDate = calDate.getDate()
+    }
+    getAPI()
+    return inputDate()
+}
 
+function calNexDate() {
+    getDate += 1
+    getDay += 1
+    calDay()
+    if (getDate > new Date(getYear, getMonth, 0).getDate()) {
+        getMonth += 1
+        getDate = 1
+    }
+    getAPI()
+    return inputDate()
+}
+
+async function removeChild() {
+    diet_arr = []
+    while (parentElement.hasChildNodes()) {
+        parentElement.removeChild(
+          parentElement.firstChild
+        )
+      }
+}
+  
+
+// 웹 적용 코드
+currentDate[0].textContent = inputDate();
+
+
+previousDate[0].addEventListener('click', async function(event) {
+    removeChild()
+    currentDate[0].textContent = calPreDate()
+})
+
+nextDate[0].addEventListener('click', async function(event) {
+    removeChild()
+    currentDate[0].textContent = calNexDate()
+})
