e57e7853404c0b3d86157b66b7c49dbea6dd268c
yddl3013 <gominjae47@gmail.com>
2022-09-23 23:00:00 +0900

    Create axios.js

---
+++ axios.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+const app = express()
+
+// const HTTP_PORT = 80
+// const HTTPS_PORT = 443
+
+// const options = {
+// 	key: fs.readFileSync('./server.key'),
+//     cert: fs.readFileSync('./server.crt'),
+//     ca: fs.readFileSync('./server.csr'),
+// };
+
+
+
+app.use(express.static(__dirname + ''))
+
+app.get('/', function(req,res) {
+    res.sendFile(__dirname + '/diet.html')
+})
+
+app.listen(80, function() {
+    console.log('express server on 80')
+})
+
+// https.createServer(options, app).listen(HTTPS_PORT);
